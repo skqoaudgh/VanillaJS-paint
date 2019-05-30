097b7ea1ebcc772c62683d78fb71f926fc19c7c6
myungho <skqoaudgh2@hanmail.net>
2019-05-31 00:12:10 +0900

    이미지 저장 기능 추가

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const ctx = canvas.getContext('2d');
 const colors = document.querySelectorAll('.jsColor');
 const range = document.getElementById('jsRange');
 const mode = document.getElementById('jsMode');
+const save = document.getElementById('jsSave');
 
 const INITIAL_COLOR = '#2c2c2c';
 const CANVAS_SIZE = 700;
 
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
@@ -66,12 +70,25 @@ function handleClickCanvas(event) {
     }
 }
 
+function handleCM(event) {
+    event.preventDefault();
+}
+
+function handleSaveClick(event) {
+    const image = canvas.toDataURL();
+    const link = document.createElement('a');
+    link.href = image;
+    link.download = 'download';
+    link.click();
+}
+
 if(canvas) {
     canvas.addEventListener('mousemove', onMouseMove);
     canvas.addEventListener('mousedown', startPainting);
     canvas.addEventListener('mouseup', stopPainting);
     canvas.addEventListener('mouseleave', stopPainting);
     canvas.addEventListener('click', handleClickCanvas);
+    canvas.addEventListener('contextmenu', handleCM);
 }
 
 colors.forEach(color => color.addEventListener('click', handleColorClick));
@@ -81,5 +98,9 @@ if(range) {
 }
 
 if(mode) {
-    mode.addEventListener('click',handleModeClick);
+    mode.addEventListener('click', handleModeClick);
+}
+
+if(save) {
+    save.addEventListener('click', handleSaveClick);
 }
