d2cd6ada8aff776f8dd57c95686582f351a51b7d
myungho <skqoaudgh2@hanmail.net>
2019-05-30 23:47:57 +0900

    선 두께 조절 기능 추가

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
 const colors = document.querySelectorAll('.jsColor');
+const range = document.getElementById('jsRange');
 
 canvas.width = 700;
 canvas.height = 700;
@@ -36,6 +37,11 @@ function handleColorClick(event) {
     ctx.strokeStyle = color;
 }
 
+function handleRangeChange(event) {
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
 if(canvas) {
     canvas.addEventListener('mousemove', onMouseMove);
     canvas.addEventListener('mousedown', startPainting);
@@ -44,3 +50,7 @@ if(canvas) {
 }
 
 colors.forEach(color => color.addEventListener('click', handleColorClick));
+
+if(range) {
+    range.addEventListener('input', handleRangeChange);
+}
